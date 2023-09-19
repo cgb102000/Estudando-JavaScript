af95ab96ca07076d462eae549b3316ef6df13588
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-09-19 19:19:21 -0300

    Create index.js

---
+++ aula07/index.js
@@ -0,0 +1,13 @@
+const nome = 'Caio'; //String
+const nome1 = "Caio"; //String
+const nome2 = `Caio`; //String
+const num1 = 10; //Number
+const num2 = 10.52; //Number
+let nomeAluno; //Undefined = Não aponta para nenhum local na memória.
+let sobrenomeAluno = null; //Nulo -> Não aponta para local nenhum na memória.
+const aprovado = true; //Boolean (lógico)
+const reprovado = false; //Boolean (lógico)
+
+const a = [1,2];
+const b = a;
+console.log(a,b);
