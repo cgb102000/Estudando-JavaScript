5decf074c182126290878ecbb03c8cc4b0225a70
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-09-21 04:15:29 -0300

    estudando denovo.

--- aula11/index.js
+++ aula11/index.js
@@ -1,1 +1,8 @@
-document.write('a');
+window.alert("Hello World"); //Alerta na tela, pode ser usada somente no DOM, ou seja, não pode ser utilizado no node.
+
+window.confirm('Deseja realmente apagar?'); //Alerta na tela, usada para confirmação do usuário.
+
+window.prompt('Digite um número;'); //Alerta na tela, capta a entrada do usuário.
+
+    
+
