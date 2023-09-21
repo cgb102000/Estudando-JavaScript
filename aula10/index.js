22e7fe13668818b25bca15c56a162b8662b9690b
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-09-21 03:27:30 -0300

    estudando...

---
+++ aula10/index.js
@@ -0,0 +1,22 @@
+const passo = 50;
+let contador = 0;
+contador += passo; //contador=contador+passo
+
+/*
+*Incremento = ++ 
+*Decremento = --
+*/
+
+//Convertendo variaveis strings em números inteiros
+
+const num1 = 10;
+const num2 = parseInt('5');
+console.log(num1+num2);
+console.log(typeof(num2));
+
+//Convertendo variaveis strings em números decimais
+
+const num3 = 10.2;
+const num4 = parseFloat('5');
+console.log(num3+num4);
+console.log(typeof(num4));
---
+++ aula10/teste.js
@@ -0,0 +1,9 @@
+console.log('Seja bem vindo!');
+
+const   idade = parseInt('23');
+const   anoNascimento    =parseInt('1999');
+
+
+console.log(typeof(idade));
+console.log(typeof(anoNascimento));
+
