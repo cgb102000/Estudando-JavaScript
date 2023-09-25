921abcba32be4f1688458653742c05754cf3f4e0
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-09-25 19:51:33 -0300

    Update index.js

--- aula14/index.js
+++ aula14/index.js
@@ -1,5 +1,13 @@
-let num1 = 1;
+let num1 = 1500;
 let num2 = 2.5;
 
 console.log(num1.toString() +num2);  //Convertendo números para strings
+//Obs: essa conversão é apenas de retorno, não altera o valor da variável;
+
+console.log(typeof num1);
+
+
+//Também podemos modificar esse retorno para números binários, usando a sintaxe a baixo:
+
+console.log(num1.toString(5)); //Apenas colocamos o (2) dentro do médoto;  Se você quiser que ele parece em uma espeficica casa decimal, deve colocar o número dentro dos parenteses, como o exemplo acima toString(5)
 
