1f5e88b40cf042095955248c0aecf1f44a0131d5
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-09-27 14:15:44 -0300

    atualizado

--- aula14/index.js
+++ aula14/index.js
@@ -19,3 +19,23 @@ console.log(typeof num1);
 
 let temp1 = num2 * 'Olá';
 console.log(Number.isNaN(temp1));
+
+
+let num3 = 0.7;
+let num4 = 0.1;
+
+num3 += num4; //0.8
+num3 += num4; //0.9
+num3 += num4; //1.0
+
+num3 = num3.toFixed(2);
+console.log(num3);
+
+console.log(Number.isInteger(num3));
+
+
+
+
+
+
+
---
+++ aula15/index.js
@@ -0,0 +1,16 @@
+//Arrendondando números para baixo
+
+let num1= 9.54578;
+let num2 = Math.floor(num1);
+console.log(num2);    //=9
+
+
+//Arredondando números para cima 
+let num3 = Math.ceil(num1);
+console.log(num3);  //=10
+
+
+
+//Arredonda o número para o valor mais próximo
+let num4 = Math.round(num1);
+console.log(num4);
