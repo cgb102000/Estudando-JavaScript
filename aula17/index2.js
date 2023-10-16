f05dbdf70e580163b0e7770389f6ff0c3d99f4ec
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-10-16 15:07:24 -0300

    raiz

--- aula17/index2.js
+++ aula17/index2.js
@@ -1,11 +1,14 @@
-function soma(x,y){
-    const resultado = x+y;
+function soma(x, y) {
+    const resultado = x + y;
     return resultado;
-
 }
 
+console.log(soma(2, 2)); // Chame a função soma e imprima o resultado diretamente
 
-console.log(2,2);
+function soma2(a = 1, b = 1) {
+    const resultado2 = a + b;
+    return resultado2;
+}
 
-//const resultado = soma (2,2);
-console.log(resultado);
+const resultado2 = soma2(4, 2);
+console.log(resultado2);
