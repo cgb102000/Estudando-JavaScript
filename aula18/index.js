c34f73174b410a4711d7afd59a1df8ada99f6272
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-10-17 13:39:22 -0300

    estudando..

---
+++ aula18/index.js
@@ -0,0 +1,7 @@
+const array = [ 1,2,3];
+array.push(4);
+array[0] = 'Caio';
+console.log(array);
+
+//Mas não podemos fazer isso, pois não podemos reatribuir um valor, apenas alterar os valores dele
+array = 5;
