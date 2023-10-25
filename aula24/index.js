c1e8bc5735082a2601d3d82a16bc3cfc77f45729
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-10-25 14:04:09 -0300

    Estudando...

--- aula24/index.js
+++ aula24/index.js
@@ -10,3 +10,16 @@ if(hora>=0 && hora<=11){
     console.log('Olá.')  
 }
 
+
+
+const tenhoGrana = true;
+if(tenhoGrana){
+    console.log('Vou sair de casa');
+
+}   else {
+    console.log('Não vou sair de casa');
+}
+
+//podemos usar condições sem else if, usando apenas if e else, também podemos usar apenas o if;
+//Sempre que usarmos else, precisamos de um if antes;
+//Podemos ter apenas um else no bloco;
--- aula24/script.js
+++ aula24/script.js
@@ -3,6 +3,8 @@ const agora = new Date();
 const hora = agora.getHours() ;
 const minutos = agora.getMinutes();
 
+
+
 if(hora>=0 && hora<=11){
     console.log('Bom dia!');
 }   else if(hora>=12 && hora <= 17){
