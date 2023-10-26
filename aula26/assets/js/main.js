dd4f3f141b9551ff8413e1f569277b0d9e236804
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-10-26 19:43:32 -0300

    estudando cada dia mais

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,41 +1,34 @@
-// IMC = PESO/ALTURA²
+/*
+<18,5 = ABAIXO DO PESO
+>18.5 && <24,9 = PESO NORMAL
+>=25 && <=29.9 = SOBREPESO
+>=30 && <=34,9 = OBESIDADE 1
+>=35 && <=39,9 = OBESIDADE 2 
+>40 = OBESIDADE 3
+*/
 
-
-const form = document.querySelector('#form');
+//CAPTURAR O EVENTO DE SUBMIT DO FORMULÁRIO;
+const form = document.querySelector('#formulario');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log('Evento não realizado!');
+    console.log('Evento não realizado !');
+    setResultado('Texto criado com sucesso!');
 
-    const inputPeso = e.target.querySelector('#peso');
-    const inputAltura = e.target.querySelector('#altura');
-
-    const peso = Number(inputPeso.value);
-    const altura = Number(inputAltura.value);
-
-
-    if (peso) {
-        console.log('ok');
-        setResultado('Peso invalido');
-
-    } else {
-        console.log('erro');
-    }
 });
 
-
-function criaP(className) {
+function criaP(className){
     const p = document.createElement('p');
+    //p.classList.add(className);
+    //p.innerHTML = 'Qualquer coisa';  //Texto que será criado no <p>
     return p;
-}
+
+};
 
 function setResultado(msg) {
     const resultado = document.querySelector('#resultado');
-    resultado.innerHTML = '';
+    resultado.innerHTML = ''; //Texto que será criado no <p>
+    //resultado.appendChild(p);
     const p = criaP;
 
-
-}
-
-
-
+};
