d9fc2636ce7679c4dc7d4736561b6d13a3920114
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-11-06 14:00:45 -0300

    update main.js

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -14,30 +14,61 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
     const inputPeso = e.target.querySelector('#peso');
     const inputAltura = e.target.querySelector('#altura');
-    
+
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
-    console.log(peso,altura)
+    console.log(peso, altura)
 
-    if (!peso){
-        setResultado('Peso inválido',false);
-        return; 
+    if (!peso) {
+        setResultado('Peso inválido', false);
+        return;
 
 
-    } 
-    if (!altura){
+    }
+    if (!altura) {
         setResultado('Altura invalida', false);
         return;
     }
-    const imc = getImc(peso,altura);
+    const imc = getImc(peso, altura);
     console.log(imc);
-    
+
 });
-function getImc (peso,altura) {
-    const imc = peso / altura ** 2 ;
+function getNivelIMC(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade nivel 1', 'Obesidade nivel 2', 'Obesidade nivel 3'];
+
+    if (imc >= 39.9) {
+        return nivel[5];
+
+    } if (imc >= 34.9) {
+        return nivel[4];
+    }
+    if (imc >= 29.9) {
+        return nivel[3];
+    }
+    if (imc >= 24.9) {
+        return nivel[2];
+    }
+    if (imc >= 18.5) {
+        return nivel[1];
+    } else if (imc < 18.5) {
+        return nivel[0];
+    }
+
+    /*
+<18,5 = ABAIXO DO PESO
+>18.5 && <24,9 = PESO NORMAL
+>=25 && <=29.9 = SOBREPESO
+>=30 && <=34,9 = OBESIDADE 1
+>=35 && <=39,9 = OBESIDADE 2 
+>40 = OBESIDADE 3
+*/
+}
+function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+
     return imc.toFixed(2);
 }
-function criaP(className){
+function criaP(className) {
     const p = document.createElement('p');
     //p.classList.add(className);
     //p.innerHTML = 'Qualquer coisa';  //Texto que será criado no <p>
@@ -52,6 +83,6 @@ function setResultado(msg, isValid) {
     const p = criaP();
     p.innerHTML = msg;
     resultado.appendChild(p);
-    
+
 
 };
