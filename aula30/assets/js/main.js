13b6efbdf7f90f925b43c8fd393c6599ffa09f34
Caio Gabriel <104166604+cgb102000@users.noreply.github.com>
2023-11-23 13:24:53 -0300

    Update main.js

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -1,4 +1,28 @@
-const data = new Date();
+
+
+
+let diaSemana = new Date();
+let diaSemanaTexto;
+
+if (diaSemana === 0) {
+    diaSemanaTexto = 'Domingo';
+} else if (diaSemana === 1) {
+    diaSemanaTexto = 'Segunda-feira';
+} else if (diaSemana === 2) {
+    diaSemanaTexto = 'Terça-feira';
+} else if (diaSemana === 3) {
+    diaSemanaTexto = 'Quarta-feira';
+} else if (diaSemana === 4) {
+    diaSemanaTexto = 'Quinta-feira';
+} else if (diaSemana === 5) {
+    diaSemanaTexto = 'Sexta-feira';
+} else if (diaSemana === 6) {
+    diaSemanaTexto = 'Sábado';
+} else {
+    diaSemanaTexto = 'Dia não encontrado';
+};
+console.log(diaSemana,diaSemanaTexto);
+
 const container = document.querySelector('.container');
 const dataMostrada = container.querySelector('p');
 dataMostrada.innerHTML = data;
